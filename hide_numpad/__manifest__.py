075b675bfc7e6cef8058a8d4971de34bb6cd76f1
Robson Goncalves <robson@popcomputadores.com.br>
2018-11-25 05:13:03 -0200

    Push hide_numpad POS

---
+++ hide_numpad/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Show/Hide NumPad',
+    'version': '1.0.0',
+    'category': 'Point Of Sale',
+    'author': 'D.Jane',
+    'sequence': 10,
+    'summary': 'Show and Hide NumPadWidget with Animations',
+    'description': "",
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/header.xml'
+    ],
+    'qweb': [
+        'static/src/xml/pos.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'installable': True,
+    'application': True,
+    'license': 'OPL-1'
+}
